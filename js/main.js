d60c36fc6382435d06a1fb0dabfb83ece4e0140e
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-16 14:01:49 -0300

    feat: ativa melhorias visuais e de desempenho

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 import { initializeApp } from "./dom.js";
 import { applyMiShiroBranding } from "./mishiro-brand.js";
+import { applyMiShiroBrandV2 } from "./mishiro-brand-v2.js";
 import { setupMiShiroPdfExports } from "./mishiro-pdf-tools.js";
+import { setupMiShiroExperienceLayer } from "./mishiro-ux.js";
 import { registerServiceWorkerUpdateFlow } from "./pwa.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await initializeApp();
   applyMiShiroBranding();
+  applyMiShiroBrandV2();
   setupMiShiroPdfExports();
+  setupMiShiroExperienceLayer();
   registerServiceWorkerUpdateFlow();
 });
