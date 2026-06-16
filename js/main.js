2a42ce62a3ac42862960d2440b545bcacb83f57f
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-16 13:02:01 -0300

    Load MiShiro branding after app initialization

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "./dom.js";
+import { applyMiShiroBranding } from "./mishiro-brand.js";
 import { registerServiceWorkerUpdateFlow } from "./pwa.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await initializeApp();
+  applyMiShiroBranding();
   registerServiceWorkerUpdateFlow();
 });
