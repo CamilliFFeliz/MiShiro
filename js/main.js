4ca63cef0e46288a45c1f0bc904b7f19f295b593
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-16 13:52:05 -0300

    Enable dual MiShiro PDF export buttons

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "./dom.js";
 import { applyMiShiroBranding } from "./mishiro-brand.js";
+import { setupMiShiroPdfExports } from "./mishiro-pdf-tools.js";
 import { registerServiceWorkerUpdateFlow } from "./pwa.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await initializeApp();
   applyMiShiroBranding();
+  setupMiShiroPdfExports();
   registerServiceWorkerUpdateFlow();
 });
