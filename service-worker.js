9f716dce4d0ae3a8f72b10ae366cf9f1cd0d552c
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-23 12:47:30 -0300

    chore: atualiza cache do orcamento redesenhado

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mishiro-clean-v11-estoque-card-compacto";
+const CACHE_NAME = "mishiro-clean-v12-orcamentos-mockup";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg",
   "./pages/orcamentos.html", "./pages/estoque.html", "./pages/agenda.html", "./pages/relatorios.html", "./pages/backup.html", "./pages/configuracoes.html",
