1eb18ab381d3d803eaf0f3330341d14c321d3cce
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-23 13:02:59 -0300

    chore: atualiza cache da tela limpa de orcamentos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mishiro-clean-v13-orcamentos-icones";
+const CACHE_NAME = "mishiro-clean-v14-orcamentos-limpo";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg",
   "./pages/orcamentos.html", "./pages/estoque.html", "./pages/agenda.html", "./pages/relatorios.html", "./pages/backup.html", "./pages/configuracoes.html",
