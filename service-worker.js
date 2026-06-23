f726216300078ae569e20abf7158833e89fcb913
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-23 12:58:01 -0300

    chore: atualiza cache dos icones de orcamento

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mishiro-clean-v12-orcamentos-mockup";
+const CACHE_NAME = "mishiro-clean-v13-orcamentos-icones";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg",
   "./pages/orcamentos.html", "./pages/estoque.html", "./pages/agenda.html", "./pages/relatorios.html", "./pages/backup.html", "./pages/configuracoes.html",
