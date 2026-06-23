022ad94f4919f9173f61cc381c939634d927439e
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-23 11:11:52 -0300

    chore: atualiza cache da correcao inicio agenda

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mishiro-clean-v6-visual-referencia";
+const CACHE_NAME = "mishiro-clean-v7-inicio-agenda";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg",
   "./pages/orcamentos.html", "./pages/estoque.html", "./pages/agenda.html", "./pages/relatorios.html", "./pages/backup.html", "./pages/configuracoes.html",
