e7c7cf2cee3e40b69a010d61dacc249665f801a6
CamilliFeliz <camilliffeliz@gmail.com>
2026-06-23 12:32:27 -0300

    chore: atualiza cache dos cards compactos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mishiro-clean-v10-estoque-card-mockup";
+const CACHE_NAME = "mishiro-clean-v11-estoque-card-compacto";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg",
   "./pages/orcamentos.html", "./pages/estoque.html", "./pages/agenda.html", "./pages/relatorios.html", "./pages/backup.html", "./pages/configuracoes.html",
